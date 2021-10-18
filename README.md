a6ae04e94ac3a06c07a686d743a2bae65ff19ff0
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2021-10-18 16:16:32 -0500

    Final touches

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# 11-note-taker
+# Homework 11: Note Taker ![badmath](https://img.shields.io/badge/-JavaScript-000000?logo=javascript)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Objective 
+
+This assignment makes a web page in express and allows you to take and store notes. The page is also deployed using Heroku, which allows for easy viewing.
+
+## Changes
+
+- Added all of necessary paths to api calls
+- Added some file/database management with fs
+- Added UUIDs to the notes to make them able to be deleted
+- Honestly, there really wasn't much to this assignment.
+
+## Links
+
+[Github](https://github.com/andrewtrudeau/11-note-taker)
+[Heroku](https://hw-11-note-taker-andrewtrudeau.herokuapp.com/)
+
+## Contact
+
+If you wish to reach me for questions, please refer to the contact information below:
+
+Github Profile: [andrewtrudeau](https://github.com/andrewtrudeau)
+
+Email Me: [andrewtrudeau3@gmail.com](mailto:andrewtrudeau3@gmail.com)
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-// Import express package
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); // Add ids to notes for management
+
+// DATABASE MANAGEMENT //
 
 const dbPath = path.join(__dirname, '/db/db.json');
 
@@ -10,17 +11,24 @@ let getDB = () => JSON.parse(fs.readFileSync(dbPath));
 
 let saveDB = (json) => fs.writeFile(dbPath, json, err => { err ? console.error(err) : console.log('Database Saved!'); });
 
+// EXPRESS //
 
-// Require the JSON file and assign it to a variable called `termData`
 const PORT = 5296;
 
 // Initialize our app variable by setting it to the value of express()
+
 const app = express();
+
+// Allow JSON parsing and setting up static paths
 
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, '/public/assets')))
 
+// Get request for accessing the pages
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// API CALLS //
 
 app.get('/api/notes', (req, res) => res.json(getDB()));
 
@@ -33,6 +41,8 @@ app.post('/api/notes', (req, res) => {
     res.redirect('/notes')
 });
 
+// BONUS: easy implementation of a delete algorithm: get data as array, remove item by ID, and save to file.
+
 app.delete('/api/notes/:id', (req, res) => {
     let json = getDB();
     let data = json.find(e => e.id === req.params.id);
@@ -42,8 +52,10 @@ app.delete('/api/notes/:id', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// Get request for accessing the pages (I only had success with this below all the other requests)
+
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(process.env.PORT || PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Listening on port ${PORT}`)
 );
